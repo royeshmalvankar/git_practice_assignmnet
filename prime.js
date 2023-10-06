7e9eac5a153968facd806f571c79bfc57c8571f6
Royesh Malvankar <royeshmalvankar@gmail.com>
2023-10-06 21:16:09 +0530

    identify prime code

---
+++ prime.js
@@ -0,0 +1,22 @@
+function prime(num)
+{
+  if(num%i===0)
+  {
+    f+=1
+  }
+  return f
+}
+f=0
+num1=12
+for (i=1;i<=num1+1;i++)
+{
+p=prime(num1)
+}
+if (p<=1)//change this number to 2 to correct the code
+{
+console.log("Prime number")
+}
+else
+{
+console.log("Composite number")    
+}
