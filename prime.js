d2edf002a211eeaea315384715e08c781675a8d3
Royesh Malvankar <royeshmalvankar@gmail.com>
2023-10-07 13:40:04 +0530

    m1 modified

--- prime.js
+++ prime.js
@@ -6,12 +6,16 @@ function prime(num)
   }
   return ft
 }
+
+
 ft=0
 num1=13
 for (i=1;i<=num1+1;i++)
 {
 pri=prime(num1)
 }
+
+
 if (pri<=2 )//change this number to 2 to correct the code
 {
 console.log("Prime number")
